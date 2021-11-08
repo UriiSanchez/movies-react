602d58b39e78f35b599922396e613ea839b821f8
uriel antonio cerón sánchez <uriel.antonio.sanchez@outlook.com>
2021-11-08 14:11:39 -0600

    sasa

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import { MovieDetails } from "./pages/MovieDetails";
 import { LandingPage } from "./pages/LandingPage";
 
 export function App() {
   return (
-    <Router> 
-      <header>
-        <Link to="/"><h1 className="title">Movies</h1></Link>
-      </header>
-      <main>
-      <main>
+      <Router> 
+        <header>
+          <Link to="/"><h1 className="title">Movies</h1></Link>
+        </header>
+        <main>
           <Switch>
             <Route exact path="/movies/:movieId"> <MovieDetails/> </Route>
             <Route path="/"> <LandingPage/> </Route>
           </Switch>
-          </main>
-      </main>
-   </Router>
+        </main>
+    </Router>
   );
 }
